f2b94df78da2d13caed4dbca63eca5b2de5c9da4
UKULHAWCZK <ulabaczko@gazeta.pl>
2021-04-06 23:00:23 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("TEST");
+        echo "# TECHNIKI-PROGRAMOWANIA-EXTRA" >> README.md
+    }
+}
+echo "# TECHNIKI-PROGRAMOWANIA-EXTRA" >> README.md
